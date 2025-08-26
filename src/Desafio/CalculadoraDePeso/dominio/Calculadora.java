dea0914c0f03705e0d795329cbc5c5556e01a4d3
unknown <batatovisk0330@gmail.com>
2025-08-25 21:28:11 -0300

    Exercicio de Enumeração

---
+++ src/Desafio/CalculadoraDePeso/dominio/Calculadora.java
@@ -0,0 +1,23 @@
+package Desafio.CalculadoraDePeso.dominio;
+
+import java.util.Scanner;
+
+public class Calculadora {
+
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+
+
+        System.out.println("\n-- Calculadora de Peso -- \n");
+
+        System.out.print(" Digite seu nome: ");
+        String nome = entrada.nextLine();
+
+        System.out.print(" Digite seu peso (kg): ");
+        int peso = entrada.nextInt();
+
+        System.out.println("[############ 100.00 %");
+
+        System.out.println("Olá "+ peso +" Kg seu peso é: "+nome);
+    }
+}
