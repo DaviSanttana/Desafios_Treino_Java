b8ed1d87919cb8bb945aeec53a6196b14da86855
unknown <batatovisk0330@gmail.com>
2025-09-04 19:02:10 -0300

    Documentação Escolar

---
+++ src/DesafiosCodeWars/DocumentacaoEscolar/DocumentacaoEscolar.java
@@ -0,0 +1,17 @@
+package DesafiosCodeWars.DocumentacaoEscolar;
+
+public class DocumentacaoEscolar {
+    public static int PaginasBranco(int colegas, int paginas) {
+        if (colegas < 0 || paginas < 0) {
+            return 0;
+        } else if (colegas > 0) {
+            return colegas * paginas;
+        }return  0 ;
+
+    }
+
+    public static void main(String[] args) {
+        int paginas1 = PaginasBranco(3,4);
+        System.out.println(paginas1);
+    }
+}
