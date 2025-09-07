9052a90d6141d1e6c640638011bf5112f10122c0
unknown <batatovisk0330@gmail.com>
2025-09-06 21:51:26 -0300

    CodeWars Desafios

---
+++ src/DesafiosCodeWars/RepeticaoDeSequencia/dominio/RepeticaoString.java
@@ -0,0 +1,12 @@
+package DesafiosCodeWars.RepeticaoDeSequencia.dominio;
+
+public class RepeticaoString {
+    public static String repeticao(final int repete, final String string) {
+        String s = " ";
+        for (int i = 0; i < repete; i++) s += string;
+        return s;
+    }
+    public static void main(String[] args) {
+        System.out.println(repeticao(4,"ass"));
+    }
+}
