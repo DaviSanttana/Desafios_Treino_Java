d506f26a4ccb30811c806a00f06d5d9a46779cf6
Davi <batatovisk0330@gmail.com>
2025-09-25 00:55:58 -0300

    tabuada com for

--- src/DesafiosLogicadeProgramacao/Tabuada/Tabuada.java
+++ src/DesafiosLogicadeProgramacao/Tabuada/Tabuada.java
@@ -3,8 +3,9 @@ package DesafiosLogicadeProgramacao.Tabuada;
 public class Tabuada {
     public static void main(String[] args) {
         for (int num1 = 1; num1 <= 9; num1++) {
+    System.out.println("\nTabuada do "+num1);
             for (int num2 = 1; num2 <=10 ; num2++) {
-                System.out.println( num1*num2 );
+                System.out.println( num1+" x "+num2+" = "+num1*num2 );
             }
         }
     }
